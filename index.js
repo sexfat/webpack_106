aae8dceef98073542ec21a08481e3d845f5af16a
sexfat <bryant6831@gmail.com>
2020-05-07 14:30:21 +0800

    add module

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 import hello from './hello';
 import content from './content';
+import $  from 'jquery';
+import TweenMax  from 'gsap'
+
+
+$('.box').css('background-color' ,"#000");
+
+
+TweenMax.to('.box' ,1 ,{x: 100}); 
+
+
+
+
+
 
 console.log(hello + content);
