73b4278d81de60d111685b4b322ae83953449069
sexfat <bryant6831@gmail.com>
2020-05-07 15:50:04 +0800

    add

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import $ from 'jquery';
 import TweenMax  from 'gsap'
 
 
-$('.box').css('background-color' ,"blue");
+$('.box').css('background-color' ,"yellow");
 
 
-TweenMax.to('.box' ,1 ,{x: 300}); 
+TweenMax.to('.box' ,1 ,{x: 400,
+y: 400}); 
 // console.log('ok');
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dest'),
+    filename: 'main.js'
   }
 };
