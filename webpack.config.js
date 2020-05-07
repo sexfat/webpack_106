e1145fd4e87e15e0d48beb6b364507df017088f9
sexfat <bryant6831@gmail.com>
2020-05-07 16:36:13 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,29 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode : 'development',//開發模式
+  mode: 'development', //開發模式
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dest'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
-  }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    open  : true,
+    port: 3000
+  },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    }, ],
+  },
+  plugins: [
+    //html 5 plugin
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
