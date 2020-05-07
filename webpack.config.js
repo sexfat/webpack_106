1c33015bc3b9a54b9101c7e519b8ddc4ff9fc13f
sexfat <bryant6831@gmail.com>
2020-05-07 15:55:42 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
+  mode : 'development',//開發模式
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dest'),
